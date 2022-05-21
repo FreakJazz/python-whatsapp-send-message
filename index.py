a2a907cf7863c538c1a7ca505ec91c34cf8ea4e0
FreakJazz <jazzrb2307@hotmail.com>
2022-05-21 13:46:06 -0500

    add dotenv

--- index.py
+++ index.py
@@ -1,13 +1,16 @@
+import os
 from twilio.rest import Client
-TWILIO_ACCOUNT_SID='AC0ccf3b91bd30a8276d87212348ae6a56'
-TWILIO_AUTH_TOKEN='9a08e831ec537e4d0b73767b3f296d35'
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()  # take environment variables from .env.
+
 # client credentials are read from TWILIO_ACCOUNT_SID and AUTH_TOKEN
-client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+client = Client(os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
 
 # this is the Twilio sandbox testing number
-from_whatsapp_number='+14155238886'
-# replace this number with your own WhatsApp Messaging number
-to_whatsapp_number='+593983551848'
+from_whatsapp_number = os.getenv('TWILIO_NUMBER')
+# NUMBER PERSONAL 
+to_whatsapp_number = os.getenv('PERSONAL_NUMBER')
 
 message = client.messages.create( 
                               from_='whatsapp:+14155238886',  
