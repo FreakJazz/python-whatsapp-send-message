4370e3edeb2c5088ff705ddfe581c8f77215adf5
FreakJazz <jazzrb2307@hotmail.com>
2022-06-24 17:31:40 -0500

    add change enter

--- pywhatkit_whatsapp.py
+++ pywhatkit_whatsapp.py
@@ -7,13 +7,14 @@ load_dotenv()  # take environment variables from .env.
 # using Exception Handling to avoid unexpected errors
 try:
      # sending message in Whatsapp in India so using Indian dial code (+91)
-     pwk.sendwhatmsg("+593964091474", "Test send message automatic", 16, 29)
-     pyautogui.press('enter')
+     pwk.sendwhatmsg("+593964091474", "Test send message automatic", 20, 54)
+     #pyautogui.press('enter')
      print("Message Sent!") #Prints success message in console
  
      # error message
 except: 
      print("Error in sending the message")
+
 
 
 
--- selenium_whatsapp.py
+++ selenium_whatsapp.py
@@ -1,7 +1,23 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-wassup_url = r'https://web.whatsapp.com/send?phone={ur_mobile_number}&text&app_absent=0'
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+# driverPath = "driver/chromedriver.exe"
+driver = webdriver.Chrome('/path/to/chromedriver')     #executable_path = driverPath
+driver.get('https://web.whatsapp.com')
 
-driver = webdriver.Chrome(r'C:\Users\jay\Desktop\PythonInOffice\amazon_price_alert_bot\chromedriver.exe')
+name = input('name: ')
+msg = input('messagge:')
+count = int(input('count: '))
 
-driver.get(wassup_url)
+user = driver.find_element_by_xpath('//span[tittle = "{}"]'.format(name))
+user.click()
+msg_box = driver.find_element_by_class_name('_2sivp')
+
+
+
+# wassup_url = r'https://web.whatsapp.com/send?phone={ur_mobile_number}&text&app_absent=0'
+
+# driver = webdriver.Chrome(r'C:\Users\jay\Desktop\PythonInOffice\amazon_price_alert_bot\chromedriver.exe')
+
+# driver.get(wassup_url)
