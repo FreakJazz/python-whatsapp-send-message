320e9f52cad323a6f2239daa243135f715f71e1e
freakjazz <jazzrb2307@hotmail.com>
2023-01-22 19:09:29 -0500

    add requirements actual

--- twilio_whatsapp.py
+++ twilio_whatsapp.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
-
+print(os.getenv('TWILIO_ACCOUNT_SID'))
 # client credentials are read from TWILIO_ACCOUNT_SID and AUTH_TOKEN
 client = Client(os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
 
@@ -13,10 +13,9 @@ from_whatsapp_number = os.getenv('TWILIO_NUMBER')
 to_whatsapp_number = os.getenv('PERSONAL_NUMBER')
 
 message = client.messages.create(
-                            from_='whatsapp:+14155238886',  
                             body='Hola como estas',      
-                            to='whatsapp:+593983551848' 
-                            ) 
+                            from_='+14155238886',  
+                            to='+593983551848') 
 
 print(message.sid)
 
